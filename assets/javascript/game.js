c174102a0bb34afada56c752eaef0132684fd523
Han Ban <hannah15198@gmail.com>
2018-04-30 10:03:42 -0700

    Added functions and called functions for the game logic.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 // Declare variables for crystals, wins, loses, number
 
 var randNum = randomNumberGenerator(19,120);
@@ -28,22 +30,26 @@ console.log (crystal1)
     // Update wins and loses
     function updateWinsLoses (){
         if (scoreCounter === parseInt(randNum)) {
+            reset();
             wins++;
             $("#wins").html(wins);
+            
         }
         else if (scoreCounter> parseInt(randNum)) {
+            reset();
             loses++;
             $("#loses").html(loses);
+            
         }
     }
     
     // Restart game 
-    function restart(){
+    function reset(){
         randNum = randomNumberGenerator(19,120);
+        $("#magic-number").html(randNum)
         scoreCounter = 0;
-        wins = 0;
-        loses = 0;
         crystal1 = randomNumberGenerator(1,12);
+        $("#score-counter").html(scoreCounter)
         crystal2 = randomNumberGenerator(1,12);
         crystal3 = randomNumberGenerator(1,12);
         crystal4 = randomNumberGenerator(1,12);
@@ -56,46 +62,41 @@ console.log (crystal1)
 
     
     $("#blue-crystal").on("click",function() {
-        crystal1 = $("#blue-crystal").attr("value", crystal1);
+        $("#blue-crystal").attr("value", crystal1);
         crystal1 = parseInt(crystal1);
         scoreCounter += crystal1;
-        $("#scoreCounter").html(scoreCounter)
+        $("#score-counter").html(scoreCounter)
+        updateWinsLoses();
     });
 
     $("#green-crystal").on("click",function() {
-        crystal2 = $("#green-crystal").attr("value", crystal2);
+        $("#green-crystal").attr("value", crystal2);
         crystal2 = parseInt(crystal2);
         scoreCounter += crystal2;
-        $("#scoreCounter").html(scoreCounter)
+        $("#score-counter").html(scoreCounter)
+        updateWinsLoses();
     });
 
     $("#red-crystal").on("click",function() {
-        crystal3 = $("#red-crystal").attr("value", crystal3);
+        $("#red-crystal").attr("value", crystal3);
         crystal3 = parseInt(crystal3);
         scoreCounter += crystal3;
-        $("#scoreCounter").html(scoreCounter)
+        $("#score-counter").html(scoreCounter)
+        updateWinsLoses();
     });
 
     $("#yellow-crystal").on("click",function() {
-        crystal4 = $("#yellow-crystal").attr("value", crystal4);
+        $("#yellow-crystal").attr("value", crystal4);
         crystal4 = parseInt(crystal4);
         scoreCounter += crystal4;
-        $("#scoreCounter").html(scoreCounter)
+        $("#score-counter").html(scoreCounter)
+        updateWinsLoses();
     });
-
-
+    
+    $("#magic-number").html(randNum)
     // If scoreCounter equals randNum, call updateWinsLoses 
 
-    if (scoreCounter === randNum) {
-        wins++;
-        $("#wins").html("wins");
-    }
-    else if (scoreCounter> randNum) {
-        loses++;
-        $("#loses").html("loses");
-        
-    }
-    restart();
+
    
     
-
+});
